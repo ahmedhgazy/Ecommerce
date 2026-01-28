5a88c5e04248b6bd955668f80b7714ba3d93317c
Ahmed Hgazy <ahmedmhgazy69@gmail.com>
2026-01-28 15:18:49 +0200

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://premium-commerce.runasp.net/api',
+  apiUrl: 'https://premium-commerce.runasp.net/api',
   googleClientId: '18506674420-q5qbocm2n9h5hdn7f3gmuqq4opoga7aq.apps.googleusercontent.com',
 
   stripePublishableKey: 'pk_test_51SnJ1aBuFmbtHlG6RUkBnZfh6aPEjepRhS6QaGRJ3gMef86h69rHHCZ2lkSh3yzkqEDe5wgPx4QAzkxTl54nB1SH007ceRtlP1'
