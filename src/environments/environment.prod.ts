2717db3ab0b0d9088f7b2ceef4192518ccef6441
Ahmed Hgazy <ahmedmhgazy69@gmail.com>
2026-01-28 14:11:16 +0200

    add production api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://your-production-api.com/api',
+    apiUrl: 'http://premium-commerce.runasp.net/api',
     googleClientId: '18506674420-q5qbocm2n9h5hdn7f3gmuqq4opoga7aq.apps.googleusercontent.com'
 };
