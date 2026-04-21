b7551076b137f3b1cf8e5edefa6bde0b836c0975
Ahmed Hgazy <ahmedmhgazy69@gmail.com>
2026-04-21 13:00:43 +0200

    update about page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,37 +12,38 @@ import { BackToTopDirective } from './shared/directives/top.directive';
 import { AuthService } from './services/auth/auth.service';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [
-        RouterOutlet,
-        HeaderComponent,
-        FooterComponent,
-        CommonModule,
-        BackToTopDirective,
-        LoadingComponent
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+    BackToTopDirective,
+    LoadingComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-    goService = inject(GoogleAuthService);
-    loading = inject(LoadingService);
-    authS = inject(AuthService);
-    router = inject(Router);
+  goService = inject(GoogleAuthService);
+  loading = inject(LoadingService);
+  authS = inject(AuthService);
+  router = inject(Router);
 
-    ngOnInit(): void {
-        this.authS.autoLogin();
-    }
+  ngOnInit(): void {
+    this.authS.autoLogin();
+  }
 
-    get hideFooter(): boolean {
-        return this.router.url.includes('/auth/login') ||
-            this.router.url.includes('/auth/register')
-            || this.router.url.includes('/profile/wishlist')
-            || this.router.url.includes('/profile');
+  get hideFooter(): boolean {
+    return this.router.url.includes('/auth/login') ||
+      this.router.url.includes('/auth/register')
+      || this.router.url.includes('/profile/wishlist')
+      || this.router.url.includes('/profile')
+      || this.router.url.includes('/products');
 
 
-    }
+  }
 
 
 }
